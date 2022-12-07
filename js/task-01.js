6a190608b32c643d4ab7e7f11fd062c1385f8cec
iamblashch <bblashchuk08@gmail.com>
2022-12-07 17:56:32 +0200

    js-6-end

--- js/task-01.js
+++ js/task-01.js
@@ -2,7 +2,7 @@ const arrayCategories = document.querySelectorAll(".item");
 
 console.log(`Number of categories: ${arrayCategories.length}`);
 
-arrayCategories.forEach((item) => {
+arrayCategories.forEach((item) =>{
   console.log(`Category: ${item.firstElementChild.textContent}`);
-  console.log(`Elemets: ${item.lastElementChild.children.length}`)
+  console.log(`Elements: ${item.lastElementChild.children.length}`);
 })
